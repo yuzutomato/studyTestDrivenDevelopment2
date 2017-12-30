0efef4971182305a26ce2e4f596c4ce57c17eccb
yuzutomato <kousanden@gmail.com>
2017-12-30 13:39:55 +0900

    nameのみを使用しているrun()を親クラスに引き上げ

--- xunit.py
+++ xunit.py
@@ -1,17 +1,17 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
-
-class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
     def run(self):
         # テストケース名を示す属性を問い合わせて、
         # 返却されたオブジェクトを関数的に呼び出す。
         # Plaggable Selectorパターン
         method = getattr(self, self.name)
         method()
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
 

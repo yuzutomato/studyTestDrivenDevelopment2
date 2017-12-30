2b5c72792c9412759ee50b8a61b59c7d05537f2d
yuzutomato <kousanden@gmail.com>
2017-12-30 20:36:49 +0900

    setUpメソッドが2つのテストの責務を担うようになったので、テスト名を変更し重複を除去

--- xunit.py
+++ xunit.py
@@ -22,12 +22,8 @@ class WasRun(TestCase):
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun('testMethod')
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert("setUp testMethod " == self.test.log)
 
-TestCaseTest('testRunning').run()
-TestCaseTest('testSetUp').run()
+TestCaseTest('testTemplateMethod').run()

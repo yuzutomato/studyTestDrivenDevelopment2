b4811634b3f3ef7bd495f4204bd4980c8466a911
yuzutomato <kousanden@gmail.com>
2017-12-30 13:24:32 +0900

    テスト対象のメソッド名を外から与える形で呼び出すよう、Plaggable Selectorパターンを使って記述

--- xunit.py
+++ xunit.py
@@ -1,8 +1,13 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
     def run(self):
-        self.testMethod()
+        # テストケース名を示す属性を問い合わせて、
+        # 返却されたオブジェクトを関数的に呼び出す。
+        # Plaggable Selectorパターン
+        method = getattr(self, self.name)
+        method()
     def testMethod(self):
         self.wasRun = 1
 

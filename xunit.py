1710085d5781564c38c4af8cd41e9f3f6fb4256d
yuzutomato <kousanden@gmail.com>
2017-12-30 13:09:24 +0900

    コンパイルエラーを除去し、テストメソッドが走ったときにwasRunフラグが立つように処理を記述

--- xunit.py
+++ xunit.py
@@ -1,3 +1,10 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
+    def testMethod(self):
+        self.wasRun = 1
+
 test = WasRun('testMethod')
 print(test.wasRun)
 test.testMethod()

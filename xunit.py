0244d9dcc6bedd016f3e7289ae487a3011083e3d
yuzutomato <kousanden@gmail.com>
2018-01-04 13:36:08 +0900

    TestSuiteのrunメソッドを追加（まだうまく動かない）

--- xunit.py
+++ xunit.py
@@ -46,6 +46,11 @@ class TestSuite:
         self.tests = []
     def add(self, test):
         self.tests.append(test)
+    def run(self):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+        return result
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):

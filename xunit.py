38fde2ab295557426c6cc1d627cefd3b355ef775
yuzutomato <kousanden@gmail.com>
2018-01-04 12:42:56 +0900

    run()からテスト結果のオブジェクトが返却されるよう修正

--- xunit.py
+++ xunit.py
@@ -13,6 +13,7 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
 class WasRun(TestCase):
     def setUp(self):
@@ -27,5 +28,14 @@ class TestCaseTest(TestCase):
         test = WasRun('testMethod')
         test.run()
         assert test.log == 'setUp testMethod tearDown '
+    def testResult(self):
+        test = WasRun('testMethod')
+        result = test.run()
+        assert result.summary() == '1 run, 0 failed'
+
+class TestResult:
+    def summary(self):
+        return '1 run, 0 failed'
 
 TestCaseTest('testTemplateMethod').run()
+TestCaseTest('testResult').run()
